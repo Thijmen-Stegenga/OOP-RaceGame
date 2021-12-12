1faf970188e44e57448fa9a1f68282b6de7f9b8c
Thijmen Stegenga <99058499@mydavinci.nl>
2021-12-12 19:03:11 +0100

    second commit
    fixed full car movement and working on more tracks

---
+++ Arcade Car (START)/Assets/Scripts/CarController.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public Rigidbody theRB;
+
+    public float forwardacceleration = 8f, reverseacceleration = 4f, maxSpeed = 50f, turnStrength = 180, gravityForce = 10f, dragDown = 3f;
+
+    private float speedInput, turnInput;
+
+    //lets the car go to the ground after a jump 
+    private bool grounded;
+
+    public LayerMask Ground;
+    public float groundRayLength = .5f;
+    public Transform groundRayPoint;
+
+    //let the wheels rotate 
+    public Transform leftFrontWheel, rightFrontWheel;
+    public float maxWheelTurn = 25f;
+
+    public ParticleSystem[] dustTrail;
+    public float maxEmission = 25f;
+    private float emissionRate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theRB.transform.parent = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speedInput = 0f;
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            speedInput = Input.GetAxis("Vertical") * forwardacceleration * 1000f;
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            speedInput = Input.GetAxis("Vertical") * reverseacceleration * 1000f;
+        }
+
+        turnInput = Input.GetAxis("Horizontal");
+
+        if (grounded)
+        {
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, turnInput * turnStrength * Time.deltaTime, 0f));
+        }
+
+        leftFrontWheel.localRotation = Quaternion.Euler(leftFrontWheel.localRotation.eulerAngles.x, (turnInput * maxWheelTurn) - 180, leftFrontWheel.localRotation.eulerAngles.z);
+        rightFrontWheel.localRotation = Quaternion.Euler(rightFrontWheel.localRotation.eulerAngles.x, turnInput * maxWheelTurn, rightFrontWheel.localRotation.eulerAngles.z);
+
+        transform.position = theRB.transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        grounded = false;
+        RaycastHit hit;
+
+        if (Physics.Raycast(groundRayPoint.position, -transform.up, out hit, groundRayLength, Ground))
+        {
+            grounded = true;
+            transform.rotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
+        }
+
+        emissionRate = 0;
+
+        if (grounded)
+        {
+            theRB.drag = dragDown;
+
+            if (Mathf.Abs(speedInput) > 0)
+            {
+                theRB.AddForce(transform.forward * speedInput);
+
+                emissionRate = maxEmission;
+            }
+        }
+        else
+        {
+            theRB.drag = 0.1f;
+            theRB.AddForce(Vector3.up * -gravityForce * 100f);
+        }
+
+        //gives particles after the car
+        foreach(ParticleSystem part in dustTrail)
+        {
+            var emissionModule = part.emission;
+            emissionModule.rateOverTime = emissionRate;
+        }
+    }
+}
